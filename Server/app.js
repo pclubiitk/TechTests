8f128be481768fbcb674bb12522d8bc31a8360cc
Yash Srivastav <yashsriv01@gmail.com>
2016-08-29 02:34:27 +0530

    Added iitk server login
    Summary:
    Uses ftp to verify user with iitk servers. Only the backend part.
    
    How to test:
    The complete login can be tested manually using `httpie`.
    Run `node app` and then test using the following commands:
    ```
    http get "localhost:8000"
    http get "localhost:8000/profile"
    ```
    ^ Will redirect to `/login`
    
    ```
    http post "localhost:8000/login" email="gangadhar@iitk.ac.in" password="shaktimaan"
    ```
    ^ If password is correct will redirect to `/profile` in the output
    
    ```
    http post "localhost:8000/login" email="gangadhar@iitk.ac.in" password="batman"
    ```
    ^ If password is wrong will redirect to `/login`
    
    Pool:
    ```
     __  __                 _   _
    |  \/  | __ _ _ __ __ _| |_| |__   __ _ ___
    | |\/| |/ _` | '__/ _` | __| '_ \ / _` / __|
    | |  | | (_| | | | (_| | |_| | | | (_| \__ \
    |_|  |_|\__,_|_|  \__,_|\__|_| |_|\__,_|___/
    ```

--- Server/app.js
+++ Server/app.js
@@ -1,22 +1,33 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var passport = require('passport');
-var session = require('express-session');
+var bodyParser   = require('body-parser');
+var config       = require('./config.js');
 var cookieParser = require('cookie-parser');
-var config = require('./config.js');
-var winston = require('winston');
+var flash        = require('connect-flash');
+var errorroutes  = require('./router.js');
+var express      = require('express');
+var logger       = require('morgan');
+var mongoose     = require('mongoose');
+var passport     = require('passport');
+var path         = require('path');
+var routes       = require('./router.js');
+var session      = require('express-session');
+var winston      = require('winston');
+var app          = express();
 
-var routes = require('./router.js');
-var errorroutes = require('./router.js');
-var __dirname = path.resolve(path.dirname());
-var app = express();
+// Connect to database
+mongoose.connect(config.db.url);
 
+// Serve static content
+app.use('/', express.static(path.join(__dirname, '../Client/')));
+
+// Load passport settings
 require('./passport')(passport);
+
+// Setup express app
 app.use(logger('short'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(session({
     secret: 'PclubIITK',
@@ -25,18 +36,20 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());
 
+// Setup routes
 app.use('/', routes);
 
+// Define 404 functions
 var error404 = function(req, res) {
     res.status(404).send('Route not found.');
 }
-
 app.get('*', error404);
 app.post('*', error404);
 
-app.listen(config.web.port, config.web.host, function () {
-  winston.log('info', 'TechTest service listening at http://%s:%s', config.web.host, config.web.port);
+app.listen(config.web.port, config.web.host, function() {
+    winston.log('info', 'TechTest service listening at http://%s:%s', config.web.host, config.web.port);
 });
 
 module.exports = app;
